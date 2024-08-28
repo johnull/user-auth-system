bede2238bdc9035406113a7228eaba06866d1860
jhng0d <johnathantostudy@gmail.com>
2024-08-28 01:06:50 -0300

    add config

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const path = require('path');
+const db = require('.config/mongoose');
 require('dotenv').config();
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.render('index', {});
+});
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
